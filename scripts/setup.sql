92f0a36c211e5566c979f273fc10e3ffcd1b74a7
Imre Angelo <imre@angelo.no>
2023-09-22 23:11:16 +0200

    Database structure

--- scripts/setup.sql
+++ scripts/setup.sql
@@ -1,14 +1,13 @@
 -- Active: 1667734353605@@127.0.0.1@3306@db
 DROP TABLE `Events`;
 CREATE TABLE Events (
-	id 		INT AUTO_INCREMENT PRIMARY KEY,
-	eid		VARCHAR(6) NOT NULL,
-	title 	VARCHAR(32) NOT NULL,
-	color 	INT 
+	id 				INT AUTO_INCREMENT PRIMARY KEY,
+	eid				VARCHAR(6) NOT NULL,
+	title 			VARCHAR(32) NOT NULL,
+	description		VARCHAR(250) DEFAULT '',
+	type 			INT DEFAULT 0,
+	address 		VARCHAR(64) DEFAULT 'TBA',
+	date			DATETIME, -- NOT NULL,
+	header			BLOB,
+	map				BLOB -- NOT NULL
 )
-
--- INSERT INTO `Events` VALUES (
--- 	1,
--- 	"Test Event #1",
--- 	0xA73121
--- )
