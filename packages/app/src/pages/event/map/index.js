43ca89ab16c9b377a91801cf0abd9b956c6c548a
Imre Angelo <imre@angelo.no>
2023-09-23 22:39:20 +0200

    Mazemap HTTPS

--- packages/app/src/pages/event/map/index.js
+++ packages/app/src/pages/event/map/index.js
@@ -13,7 +13,7 @@ export default function Map(props) {
 
 	return (
 		<div id='mazemap-container' className={s.map}>
-			<iframe src="http://use.mazemap.com/embed.html?center=10.404761,63.415742&zoom=16&zlevel=1&campusid=1?newtablink=outside"
+			<iframe src="https://use.mazemap.com/embed.html?center=10.404761,63.415742&zoom=16&zlevel=1&campusid=1?newtablink=outside"
 			width="100%" height="118%" frameBorder="0" marginHeight="0" marginWidth="0"
 			scrolling="no"></iframe>
 		</div>
