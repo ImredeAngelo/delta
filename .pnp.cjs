ae35d47ed99d664a0d885906a76c4df6f0eed39f
Imre Angelo <imre@angelo.no>
2023-10-07 20:26:43 +0200

    Jest config

--- .pnp.cjs
+++ .pnp.cjs
@@ -43,6 +43,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         [null, {\
           "packageLocation": "./",\
           "packageDependencies": [\
+            ["jest-teamcity", "npm:1.11.0"]\
           ],\
           "linkType": "SOFT"\
         }]\
@@ -3924,7 +3925,8 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         ["workspace:.", {\
           "packageLocation": "./",\
           "packageDependencies": [\
-            ["@delta/deltahouse", "workspace:."]\
+            ["@delta/deltahouse", "workspace:."],\
+            ["jest-teamcity", "npm:1.11.0"]\
           ],\
           "linkType": "SOFT"\
         }]\
@@ -3996,7 +3998,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["cors", "npm:2.8.5"],\
             ["express", "npm:4.18.2"],\
             ["jest", "virtual:1e8e096b06e126dfc40add58cb63f5bd53f3d9916ef5e549d3e524b91ca3af213ea5245e7ebfb2670a9f9fc91f1850b9e1edbad896f8a63cbd92c8be82c199b5#npm:29.7.0"],\
-            ["jest-teamcity", "npm:1.11.0"],\
             ["jose", "npm:4.14.6"],\
             ["mysql2", "npm:3.6.1"],\
             ["node-gyp", "npm:9.4.0"],\
