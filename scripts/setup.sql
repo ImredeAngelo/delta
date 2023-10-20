5f568b00af7cb4f379611d36bff0b24a71e3e1b8
Imre Angelo <imre@angelo.no>
2023-10-21 00:06:22 +0200

    Larger password size in DB

--- scripts/setup.sql
+++ scripts/setup.sql
@@ -1,4 +1,5 @@
--- DROP TABLE `Events`;
+USE db; -- TODO: Use v0 etc.
+
 CREATE TABLE Events (
         id                      VARCHAR(6) NOT NULL UNIQUE PRIMARY KEY,
         title                   VARCHAR(32) NOT NULL,
@@ -13,7 +14,7 @@ CREATE TABLE Events (
 CREATE TABLE Users (
         id                      VARCHAR(6) NOT NULL UNIQUE PRIMARY KEY,
         mail                    VARCHAR(255) UNIQUE,
-        password                VARCHAR(64) NOT NULL,
+        password                VARCHAR(128) NOT NULL,
         firstname               VARCHAR(32),
         lastname                VARCHAR(32)
 );
