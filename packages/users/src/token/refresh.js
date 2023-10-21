61e6d46d9db207087eea2f374c0bb09db6c74ede
Imre Angelo <imre@angelo.no>
2023-10-21 12:54:48 +0200

    Small refactor

--- packages/users/src/token/refresh.js
+++ packages/users/src/token/refresh.js
@@ -3,26 +3,25 @@ const { token } = require(".");
 module.exports = function(req, res) {
 	const jwt = req.cookies.token;
 
-	if(jwt) {
-		token.verify(jwt)
-			.then(r => {
-				const { id } = r.payload;
-
-				res.status(200)
-				.send({
-					status:"success",
-					user:{
-						id:id
-					}
-				})
-			})
-			.catch(e => {
-				// next();
-				res.status(401).send({ status:"failed" })
-			})
-
+	if(!jwt) {
+		res.status(200).send({ status:"failed", reason:"No credentials" })
 		return;
 	}
 
-	res.status(200).send({ status:"failed", reason:"No credentials" })
+	token.verify(jwt)
+		.then(r => {
+			const { id } = r.payload;
+
+			res.status(200)
+			.send({
+				status:"success",
+				user:{
+					id:id
+				}
+			})
+		})
+		.catch(e => {
+			res.status(401).send({ status:"failed" })
+			// next();
+		})
 }
